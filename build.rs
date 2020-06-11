e10d9f78eccdd031d5f61c02346da00fdaed240c
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-06-11 17:03:50 +0300

    build scripts for different backends

--- build.rs
+++ build.rs
@@ -2,16 +2,26 @@ use std::process::Command;
 use std::env;
 // use std::path::Path;
 
+#[cfg(feature = "adx_backend")]
+static BUILD_SCRIPT_NAME: &'static str = "make_adx.sh";
+
+#[cfg(feature = "asm_backend")]
+static BUILD_SCRIPT_NAME: &'static str = "make_asm.sh";
+
+#[cfg(all(not(feature = "adx_backend"), not(feature = "asm_backend")))]
+static BUILD_SCRIPT_NAME: &'static str = "make.sh";
+
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=make.sh");
+    println!("{}", format!("cargo:rerun-if-changed={}", BUILD_SCRIPT_NAME));
 
     let out_dir = env::var("OUT_DIR").unwrap();
-    Command::new("sh").args(&["./make.sh"]).status().unwrap();
+    let arg = format!("./{}", BUILD_SCRIPT_NAME);
+    Command::new("sh").args(&[arg]).status().unwrap();
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=eip2537");
 
     Command::new("touch").args(&["build.rs"]).status().unwrap();
-    Command::new("touch").args(&["make.sh"]).status().unwrap();
+    Command::new("touch").args(&[BUILD_SCRIPT_NAME]).status().unwrap();
 }
---
+++ make_asm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd ./eip2537go
+rm go.sum
+# go mod edit --replace github.com/ethereum/go-ethereum=github.com/kilic/go-ethereum@bls_precompiles
+go get -u
+make static_external_asm
